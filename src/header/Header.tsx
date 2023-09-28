21d03ffe0c5bd3e5746970e533dba117014d800c
JoergWolff <neuerfisch@wolffclan.de>
2023-09-28 09:31:47 +0200

    Create profile component

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,8 +1,10 @@
 import './Header.css'
+import Profile from "../profile/Profile.tsx";
 export default function Header(){
     return (
         <header className="blog-header">
             MyBlog
+            <Profile/>
         </header>
     )
 }
---
+++ src/profile/Profile.tsx
@@ -0,0 +1,12 @@
+import "./Profile.css"
+
+export default function Profile() {
+    return (
+        <div className="profile">
+            <p>Jörg Wolff</p>
+            <p>Fachinformatiker</p>
+            <p>MCSE</p>
+            <p>Web Developer</p>
+        </div>
+    )
+}
